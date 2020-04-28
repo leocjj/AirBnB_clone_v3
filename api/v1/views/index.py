dd2318950092e0d17a7a6cf586b414f786a2dcd0
kael <cdcp-1795@hotmail.com>
2020-04-28 22:39:10 +0000

    [Create] task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-dddd
+Module to handle endpoints (routes)
+that will return results from API.
 """
 
 from api.v1.views import app_views
@@ -10,6 +11,26 @@ from flask import jsonify
 @app_views.route("/status")
 def status_ok():
     """
-    pending
+    Return the status of the API:
     """
     return jsonify(status="OK")
+
+@app_views.route("/stats")
+def stats_quantity():
+    """
+    pending
+    """
+    names = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+        }
+
+    data_count = {}
+
+    for k, name in names.items():
+        data_count[name] = storage.count(k)
+    return jsonify(data_count)
