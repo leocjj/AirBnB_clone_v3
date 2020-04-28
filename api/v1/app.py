42c3e5cce336b779ba6180739ffab2b13be1562e
leocjj <leocj@hotmail.com>
2020-04-28 18:37:36 -0500

    Task 06

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ dddd
 """
 from flask import Flask
 from models import storage
+from flask import jsonify
 from os import getenv
 from api.v1.views import app_views
 
@@ -12,9 +13,10 @@ app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
-@app_views.errorhandler(404)
+@app.errorhandler(404)
 def resource_not_found(error):
     return (jsonify(error="Not found"), 404)
+
 
 @app.teardown_appcontext
 def off_sesssion(_):
@@ -25,4 +27,3 @@ if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
     app.run(host, port, threaded=True)
-
